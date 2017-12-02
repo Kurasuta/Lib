31b4d97b7f0e4876aca1c60160512e2ec954b1f9
Lars Wallenborn <lars@wallenborn.net>
2017-12-02 12:44:04 +0100

    quickfix import issue

--- general.py
+++ general.py
@@ -5,7 +5,6 @@ import os
 import errno
 from datetime import datetime
 from lib.sample import SampleMeta
-from psycopg2.extras import Json
 
 
 class DateTimeEncoder(json.JSONEncoder):
@@ -109,6 +108,8 @@ class KurasutaDatabase(object):
         """
         :type meta: SampleMeta
         """
+        from psycopg2.extras import Json
+
         payload = meta.to_dict()
         payload['hash_sha256'] = hash_sha256
         with self.connection.cursor() as cursor:
