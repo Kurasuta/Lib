3c6e077b1be3021a83234d73977b81ad03f69c29
Lars Wallenborn <lars@wallenborn.net>
2017-12-26 04:05:25 +0100

    include sections in output, implements filter for section hash

--- repository.py
+++ repository.py
@@ -15,10 +15,27 @@ class SampleRepository(object):
             if len(self.allowed_source_ids) != len(allowed_source_identifiers):
                 raise Exception('At least one source identifier is missing in database')
 
+    def by_section_hash(self, sha256):
+        with self.db.cursor() as cursor:
+            cursor.execute('''
+                SELECT sample.hash_sha256
+                FROM section
+                LEFT JOIN sample ON (sample.id = section.sample_id)
+                LFET JOIN sample_has_source ON (sample.id = sample_has_source.sample_id)
+                WHERE (section.hash_sha256 = %s) AND (sample_has_source.source_id in %s)
+            ''', (sha256, self.allowed_source_ids))
+            ret = []
+            for row in cursor.fetchall():
+                sample = Sample()
+                sample.hash_sha256 = row[0]
+                ret.append(sample)
+            return ret
+
     def by_hash_sha256(self, sha256):
         with self.db.cursor() as cursor:
             cursor.execute('''
                 SELECT
+                    sample.id,
                     sample.hash_sha256,
                     sample.hash_md5,
                     sample.hash_sha1,
@@ -46,21 +63,26 @@ class SampleRepository(object):
             row = cursor.fetchone()
             if not row:
                 return None
-            sample.hash_sha256 = row[0]
-            sample.hash_md5 = row[1]
-            sample.hash_sha1 = row[2]
-            sample.size = row[3]
-            sample.ssdeep = row[4]
-            sample.imphash = row[5]
-            sample.entropy = row[6]
-            sample.file_size = row[7]
-            sample.entry_point = row[8]
-            sample.overlay_sha256 = row[9]
-            sample.overlay_size = row[10]
-            sample.overlay_ssdeep = row[11]
-            sample.overlay_entropy = row[12]
-            sample.build_timestamp = row[13]
-            sample.strings_count_of_length_at_least_10 = row[14]
-            sample.strings_count = row[15]
+            sample_id, sample.hash_sha256, sample.hash_md5, sample.hash_sha1, sample.size, sample.ssdeep, sample.imphash, \
+            sample.entropy, sample.file_size, sample.entry_point, sample.overlay_sha256, sample.overlay_size, \
+            sample.overlay_ssdeep, sample.overlay_entropy, sample.build_timestamp, \
+            sample.strings_count_of_length_at_least_10, sample.strings_count = row
+
+            cursor.execute('''
+                SELECT
+                    s.hash_sha256,
+                    sn.content AS name,
+                    s.virtual_address,
+                    s.virtual_size,
+                    s.raw_size,
+                    s.entropy,
+                    s.ssdeep
+                FROM section s
+                LEFT JOIN section_name sn ON (s.name_id = sn.id)
+                WHERE (s.sample_id = %s)
+                ORDER BY s.sort_order
+            ''', (sample_id,))
+            for row in cursor.fetchall():
+                sample.sections.append(self.factory.create_section(*row))
 
             return sample
