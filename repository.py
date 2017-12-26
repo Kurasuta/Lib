e4561d8b143ce4be4f395ae6e0e1c683a3fee8c2
Lars Wallenborn <lars@wallenborn.net>
2017-12-26 03:03:25 +0100

    implements filtering by source

--- repository.py
+++ repository.py
@@ -2,9 +2,18 @@ from sample import SampleFactory, Sample
 
 
 class SampleRepository(object):
-    def __init__(self, db):
+    def __init__(self, db, allowed_source_identifiers):
         self.db = db
         self.factory = SampleFactory()
+
+        with self.db.cursor() as cursor:
+            cursor.execute(
+                'SELECT id FROM sample_source WHERE (identifier IN %s)',
+                (tuple(allowed_source_identifiers),)
+            )
+            self.allowed_source_ids = tuple([int(row[0]) for row in cursor.fetchall()])
+            if len(self.allowed_source_ids) != len(allowed_source_identifiers):
+                raise Exception('At least one source identifier is missing in database')
 
     def by_hash_sha256(self, sha256):
         with self.db.cursor() as cursor:
@@ -27,8 +36,9 @@ class SampleRepository(object):
                     sample.strings_count_of_length_at_least_10,
                     sample.strings_count
                 FROM sample
-                WHERE (sample.hash_sha256 = %s)
-            ''', sha256)
+                LEFT JOIN sample_has_source ON (sample.id = sample_has_source.sample_id)
+                WHERE (sample.hash_sha256 = %s) AND (sample_has_source.source_id IN %s)
+            ''', (sha256, self.allowed_source_ids))
 
             # TODO join more tables and propagate to sample object
 
